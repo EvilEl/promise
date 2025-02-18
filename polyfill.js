2bb17f10791c5ce6ffdf86212d7491c1ca4c1c53
evilel <encoru@rambler.ru>
2025-02-19 00:34:28 +0300

    feat: added polyfill allSettled

--- polyfill.js
+++ polyfill.js
@@ -61,6 +61,29 @@ Promise.any = (promises) => {
   });
 };
 
+Promise.allSettled = (promises) => {
+  if (!Array.isArray(promises)) {
+    throw new Error("not array");
+  }
+  const results = [];
+  return new Promise((res) => {
+    promises.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then((responce) => {
+          results[index] = { status: "fulfilled", value: responce };
+        })
+        .catch((err) => {
+          results[index] = { status: "rejected", value: err };
+        })
+        .finally(() => {
+          if (results.length === promises.length) {
+            res(results);
+          }
+        });
+    });
+  });
+};
+
 Promise.all([
   createLimitPromise({ value: 3, limit: 500 }),
   createLimitPromise({ value: 2, limit: 1 }),
@@ -90,3 +113,25 @@ Promise.any([
   createLimitPromise({ value: 3, limit: 333, isError: true }),
   createLimitPromise({ value: 2, limit: 555, isError: true }),
 ]).catch(console.log);
+
+const onePromiseObject = {
+  limit: 100,
+  value: "test 2 limit 100 err",
+  isError: true,
+};
+const twoPromiseObject = {
+  limit: 0,
+  value: "test 2 limit 0 suc",
+  isError: false,
+};
+const threePromiseObject = {
+  limit: 5000,
+  value: "test 2 limit 5000 suc",
+  isError: false,
+};
+
+Promise.allSettled([
+  createLimitPromise(onePromiseObject),
+  createLimitPromise(twoPromiseObject),
+  createLimitPromise(threePromiseObject),
+]).then(console.log);
